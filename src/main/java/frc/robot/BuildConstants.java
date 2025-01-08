a2601a34c1c7f4c889bf5c6aac806d92a98704b0
abhi-team2342 <abhinav.khandeshi@team2342.org>
2025-01-08 08:20:44 -0500

    more pathplanner updates

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Build2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "aa7dcc452cf5815dc1245b03a4a23493c528e5a7";
-  public static final String GIT_DATE = "2025-01-06 21:10:49 EST";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "7215a1351f0c4db758aac654859952fb577cbc3f";
+  public static final String GIT_DATE = "2025-01-08 08:19:12 EST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2025-01-07 20:10:24 EST";
-  public static final long BUILD_UNIX_TIME = 1736298624989L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-08 08:19:57 EST";
+  public static final long BUILD_UNIX_TIME = 1736342397866L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
