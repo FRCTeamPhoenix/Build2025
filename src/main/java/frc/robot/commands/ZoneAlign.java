c65890a9f0b062a55d15b48e3fb9ab701eb00c9c
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2025-01-21 20:07:41 -0500

    zonealigning works

--- src/main/java/frc/robot/commands/ZoneAlign.java
+++ src/main/java/frc/robot/commands/ZoneAlign.java
@@ -8,7 +8,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants.VisionConstants.PathfindingConstants;
+import frc.robot.Constants.PathfindingConstants;
 import frc.robot.util.PhoenixUtils;
 
 public class ZoneAlign extends Command {
