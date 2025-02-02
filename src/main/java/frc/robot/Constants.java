20a34a6fc0d50affdedf157f8dca0f37537d7bf7
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2025-02-02 15:18:26 -0500

    updating sprocket pitch for elevator

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -186,7 +186,7 @@ public final class Constants {
   public static final class ElevatorConstants {
     public static final double GEAR_RATIO = 5.0;
     public static final double CARRIAGE_MASS = Units.lbsToKilograms(25.0);
-    public static final double PULLEY_RADIUS = Units.inchesToMeters(1.75) / 2;
+    public static final double PULLEY_RADIUS = Units.inchesToMeters(1.751) / 2;
     public static final double MIN_HEIGHT = Units.inchesToMeters(37.5);
     public static final double MAX_HEIGHT = Units.inchesToMeters(85.5);
     public static final double[] POSITIONS = { 0, 0.4, 0.8, 1.2};
