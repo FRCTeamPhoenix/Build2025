577aab704a5bbed7c943427f17dff6d4412c0159
Software2342 <159857809+Software2342@users.noreply.github.com>
2025-02-08 12:04:32 -0500

    Made wrapper functions for the candle animations.

--- src/main/java/frc/robot/subsystems/candle/candle.java
+++ src/main/java/frc/robot/subsystems/candle/candle.java
@@ -1,14 +1,27 @@
 package frc.robot.subsystems.candle;
+
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.Animation;
+import com.ctre.phoenix.ErrorCode;
 import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.CANdleFaults;
+import com.ctre.phoenix.led.FireAnimation;
+import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.RgbFadeAnimation;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANdleConstants;
 
-public class Candle extends SubsystemBase{
+public class Candle extends SubsystemBase {
     private final CANdle candle; 
     private final CANdleConfiguration config;
+
+    private FireAnimation fireAnimation = new FireAnimation(1, 0.25, 300, 1, 0 );
+    private RainbowAnimation rainbowAnimation = new RainbowAnimation(1, 0.5, 300);
+    private RgbFadeAnimation rgbFadeAnimation = new RgbFadeAnimation(1, 0.25, 300 );
+
+
 
     /**
      * Creates a new CANdle object with the supplied CANID
@@ -44,5 +57,50 @@ public class Candle extends SubsystemBase{
             setColor(CANdleConstants.COLOR_OFF, CANdleConstants.COLOR_OFF, CANdleConstants.COLOR_OFF);
             Timer.delay(durationSeconds);
         }
+    }
+
+    /**
+     * Takes in an animation (probably something different than the presets) and makes candle to do it 
+     * @param animation Animation that candle will do 
+     */
+    public void animate(Animation animation) {
+        candle.animate(animation);
+    }
+
+    /**
+     * Updates last error from candle 
+     */
+    public ErrorCode getLastError() {
+        return candle.getLastError();
+    }
+
+    /**
+     * Updates faults from candle 
+     */
+    public ErrorCode getFaults(CANdleFaults faults) {
+        return candle.getFaults(faults);
+    }
+
+    
+    /**
+     * Uses preset fire animation and makes candle do it
+     */
+    public void fireAnimate() {
+        candle.animate(fireAnimation);
+    }
+
+    /**
+     * Uses preset rainbow animation and makes candle do it
+     */
+    public void rainbowAnimate() {
+        candle.animate(rainbowAnimation);
+    }
+
+    /**
+     * Uses preset rgbFade animation and makes candle do it
+     */
+    public void rgbFade() {
+        candle.animate(rgbFadeAnimation);
+
     }
 }
