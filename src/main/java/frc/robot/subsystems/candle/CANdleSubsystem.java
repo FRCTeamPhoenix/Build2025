dced9b25628e60aabbff325bc0c5252d52c89e42
Luddo183 <93882319+Luddo183@users.noreply.github.com>
2025-03-22 10:02:18 -0400

    Update at 'Sat Mar 22 10:02:18 EDT 2025'

--- src/main/java/frc/robot/subsystems/candle/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/candle/CANdleSubsystem.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems.candle;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.FieldConstants;
 import frc.robot.subsystems.candle.CANdleIO.CANdleState;
@@ -57,15 +58,14 @@ public class CANdleSubsystem extends SubsystemBase {
         }
       }
     } else if (DriverStation.isAutonomousEnabled()) {
-      candle.setMode(CANdleState.RainbowAnimation);
-    } else {
-      // if (DriverStation.getAlliance().isPresent()
-      //     && DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red) {
-      //   candle.setMode(CANdleState.Red);
-      // } else {
-      //   candle.setMode(CANdleState.Blue);
-      // }
       candle.setMode(CANdleState.LarsonAnimation);
+    } else {
+      if (DriverStation.getAlliance().isPresent()
+          && DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red) {
+        candle.setMode(CANdleState.Red);
+      } else {
+        candle.setMode(CANdleState.Blue);
+      }
     }
   }
 
